63576babfaa65528cc86324f283bdb2b658f8700
arun.prabhu <arun.prabhu@mobilelive.ca>
2020-10-27 15:57:06 -0400

    Step-3: Fetching Movies with a Traditional Promise

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-export const fetchMovies = () => {};
+import { fetchWithTimeout } from "./services";
+
+const movies = require("/data/movies.json");
+
+export const fetchMovies = () => {
+  const resolveFunction = () => movies;
+  fetchWithTimeout(1000).then(resolveFunction);
+};
+
+const moviePromise = fetchMovies();
+
+moviePromise.then((results) => {
+  console.log(results);
+});
